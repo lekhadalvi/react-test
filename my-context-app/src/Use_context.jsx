bdcc8b51e957f34a8e7551718a98441b36ffb9ad
Lekha <lekhadalvi08@gmail.com>
2025-12-06 10:54:36 +0530

    create dnew project

---
+++ my-context-app/src/Use_context.jsx
@@ -0,0 +1,17 @@
+import React, {  createContext, useState } from 'react'
+
+
+const context = createContext();
+
+const Use_context = ( {children}) => {
+    const [user , setuser] =useState("guest");
+  return (
+    <>
+    <Context.Provider value={{user,setuser}}>
+         {children}
+        </Context.Provider>    
+    </>
+  )
+}
+
+export default Use_context
