b1fcb9c064a3a324a27298c3ef2e6bb85ed84b85
Lekha <lekhadalvi08@gmail.com>
2025-12-15 15:48:35 +0530

    created api's

--- practiceapi/src/API/URL.js
+++ practiceapi/src/API/URL.js
@@ -1,1 +1,8 @@
-export const URL = axios.create 
+import axios from "axios";
+
+export const url = axios.create(
+    {
+    baseURL:"https://69316d8911a8738467cec1b0.mockapi.io/lekha/api/test"
+
+} 
+)
